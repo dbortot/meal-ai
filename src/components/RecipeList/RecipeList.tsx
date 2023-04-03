fe1617795431225c2a2fe92c6e0270b086b0f6c2
dbortot <davide.bortot94@gmail.com>
2023-04-03 18:18:17 +0100

    Test filtering data by category

--- src/components/RecipeList/RecipeList.tsx
+++ src/components/RecipeList/RecipeList.tsx
@@ -16,8 +16,8 @@ export default function RecipeList() {
 
   const _ = require('lodash');
 
-  const getThumbnailForAccordion = (Category: string) : any[] => {
-    return _.shuffle(itemData).slice(0, 3);
+  const getThumbnailForAccordion = (category: string) : any[] => {
+    return data.filter((data) => data.category === category).slice(0, 3);
   }
 
   return (
@@ -30,7 +30,7 @@ export default function RecipeList() {
         >
           <Typography>Pasta</Typography>
           <ImageList>
-            {getThumbnailForAccordion("Pasta").map((item) => (
+            {getThumbnailForAccordion("pasta").map((item) => (
               <ImageListItem key={item.img}>
                 <img
                   src={`${item.img}?w=80&fit=crop&auto=format`}
@@ -48,7 +48,7 @@ export default function RecipeList() {
             <ImageListItem key="Subheader" cols={2}>
               <ListSubheader component="div">December</ListSubheader>
             </ImageListItem>
-            {itemData.map((item) => (
+            {data.map((item) => (
               <ImageListItem key={item.img}>
                 <img
                   src={`${item.img}?w=248&fit=crop&auto=format`}
@@ -80,13 +80,26 @@ export default function RecipeList() {
           id="panel2a-header"
         >
           <Typography>Carne</Typography>
+          <ImageList>
+            {getThumbnailForAccordion("carne").map((item) => (
+              <ImageListItem key={item.img}>
+                <img
+                  src={`${item.img}?w=80&fit=crop&auto=format`}
+                  srcSet={`${item.img}?w=80&fit=crop&auto=format`}
+                  alt={item.title}
+                  loading="lazy"
+                />
+              </ImageListItem>
+
+            ))}
+          </ImageList>
         </AccordionSummary>
         <AccordionDetails>
           <ImageList sx={{ width: 500 }}>
             <ImageListItem key="Subheader" cols={2}>
               <ListSubheader component="div">December</ListSubheader>
             </ImageListItem>
-            {itemData.map((item) => (
+            {data.map((item) => (
               <ImageListItem key={item.img}>
                 <img
                   src={`${item.img}?w=248&fit=crop&auto=format`}
@@ -117,53 +130,35 @@ export default function RecipeList() {
   );
 }
 
-const itemData = [
+const data = [
   {
     img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
     title: 'Breakfast',
+    category: 'pasta'
   },
   {
     img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
     title: 'Burger',
+    category: 'pasta'
   },
   {
     img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
     title: 'Camera',
+    category: 'pasta'
   },
   {
     img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
     title: 'Coffee',
+    category: 'carne'
   },
   {
     img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
     title: 'Hats',
+    category: 'carne'
   },
   {
     img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
     title: 'Honey',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
-    title: 'Basketball',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
-    title: 'Fern',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
-    title: 'Mushrooms',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
-    title: 'Tomato basil',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
-    title: 'Sea star',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
-    title: 'Bike',
-  },
-];
+    category: 'carne'
+  }
+]
